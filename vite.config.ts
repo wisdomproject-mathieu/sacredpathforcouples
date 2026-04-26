f9ef9dbd7b37e78ef5804ddc821ffe93e141e844
Mathieu Escande <mathieu.escande@gmail.com>
2026-04-26 14:33:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -121,7 +121,7 @@ const sacredVoiceDevProxy = (): Plugin => ({
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/-sacredpath-mainstream/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
